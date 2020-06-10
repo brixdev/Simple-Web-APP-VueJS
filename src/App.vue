2283af7eae077d63a179867a83d238d1e95714cd
nessia.brb <nessia.brb@sprobe.com>
2020-06-10 18:05:51 +0800

    Sample Web Page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <!-- <Header /> -->
     <Router />
-    <!-- <Home /> -->
   </div>
 </template>
 <script>
-// import Home from "./modules/home/Home.vue";
-// import Header from "./components/Header.vue";
 import Router from "./router/Router";
 export default {
   name: "navbar",
   components: {
-    // Header,
     Router,
-    // Home,
   },
 };
 </script>
